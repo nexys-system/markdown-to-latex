3ecc7bf60d27df15e9c1e028d6ead94f3692c5e9
John B <john@nexys.ch>
2022-01-01 22:31:22 +0100

    basic package

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,22 @@
-test("dummy", () => {
-  expect(2 + 2).toEqual(4);
+import * as I from "./index";
+
+test("line to latex", () => {
+  expect(
+    I.lineToLatex(
+      "my normal _line_, this is **bold** and here is a [link](https://example.com)"
+    )
+  ).toEqual(
+    "my normal \\textit{line}, this is \\textbf{bold} and here is a \\href{https://example.com}{link}"
+  );
+
+  expect(I.lineToLatex("* list item")).toEqual("\\item list item");
+});
+
+test("to latex", () => {
+  expect(
+    I.toTex(`intro
+* item 1
+* item 2
+and some new text`)
+  ).toEqual("");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-export default "hello world";
+const splitLinesIntoArray = (s: string): string[] => s.split(/\n/g);
+
+export const lineToLatex = (
+  s: string
+): string | { list: boolean; content: string } => {
+  const itemMatch = s.match(/^\* (.+)/);
+  if (itemMatch) {
+    return { list: true, content: "  \\item " + itemMatch[1] };
+  }
+
+  return (
+    s
+      // italic
+      .replace(/_([^_]+)_/g, "\\textit{$1}")
+      // bold
+      .replace(/\*\*([^\*]+)\*\*/g, "\\textbf{$1}")
+      // link
+      .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, "\\href{$2}{$1}")
+  );
+};
+
+export const toTex = (s: string) => {
+  const arr = splitLinesIntoArray(s);
+
+  let isList = false;
+
+  return arr
+    .map((x) => {
+      const r = lineToLatex(x);
+
+      if (typeof r !== "string") {
+        if (isList === false) {
+          isList = true;
+          return ["\\begin{itemize}", r.content].join("\n");
+        }
+
+        return r.content;
+      }
+
+      if (isList === true) {
+        isList = false;
+        return ["\\end{itemize}", r].join("\n");
+      }
+
+      return r;
+    })
+    .join("\n");
+};
+
+export default toTex;
